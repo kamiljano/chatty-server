c0f435d84dd6358555ab1b8a2a9c6ea365c2fa01
Kamil Janowski <kamil.janowski@f-secure.com>
2018-09-15 16:12:35 +0300

    support for messaging

--- test/lib/userMock.js
+++ test/lib/userMock.js
@@ -6,50 +6,28 @@ const io = require('socket.io-client');
 class User {
 
   constructor(username, cookieJar, urlBase, socket) {
-    this.username = username;
     this.cookieJar = cookieJar;
     this.urlBase = urlBase;
-    this.socket = socket;
   }
 
   getCurrentSession() {
-    return request.get(`${this.urlBase}/users/~`, {
+    return request.get(`${this.urlBase}/api/v1/users/~`, {
       json: true,
       jar: this.cookieJar
     });
-  }
-
-  onPrivateMessage(callback) {
-    this.socket.on(this.username, callback);
-  }
-
-  sendPrivateMessage(targetUsername, message) {
-    return this.socket.emit('privateChat', {
-      body: message,
-      recipient: targetUsername
-    });
-  }
-
-  close() {
-    this.socket.close();
   }
 }
 
 module.exports.createUser = async (urlBase, username) => {
   const cookieJar = request.jar();
-  const response = await request.post(`${urlBase}/users`, {
+  const response = await request.post(`${urlBase}/api/v1/users`, {
     json: true,
     resolveWithFullResponse: true,
     body: {username},
     jar: cookieJar
   });
-  const socket = io.connect(urlBase);
-  await new Promise(resolve => socket.on('connect', resolve));
-  socket.on('disconnect', () => {
-    console.info('Disconnected from the pubsub');
-  });
   return {
     response,
-    user: new User(username, cookieJar, urlBase, socket)
+    user: new User(username, cookieJar, urlBase)
   }
 };
